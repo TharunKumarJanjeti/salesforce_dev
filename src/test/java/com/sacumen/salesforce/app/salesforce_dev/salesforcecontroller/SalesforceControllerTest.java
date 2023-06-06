aba8c8ce15f672da74ad81bc09f232027e782924
TharunKumarJanjeti <135584992+TharunKumarJanjeti@users.noreply.github.com>
2023-06-06 15:55:16 +0530

    Add files via upload

---
+++ src/test/java/com/sacumen/salesforce/app/salesforce_dev/salesforcecontroller/SalesforceControllerTest.java
@@ -0,0 +1,45 @@
+package com.sacumen.salesforce.app.salesforce_dev.salesforcecontroller;
+
+
+import com.force.api.ForceApi;
+import com.github.tomakehurst.wiremock.junit5.WireMockRuntimeInfo;
+import com.github.tomakehurst.wiremock.junit5.WireMockTest;
+import org.junit.jupiter.api.Test;
+import org.mockito.Mock;
+import org.springframework.boot.test.web.client.TestRestTemplate;
+
+import static com.github.tomakehurst.wiremock.client.WireMock.*;
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+@WireMockTest(httpsEnabled = true, httpsPort = 8080)//@SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
+public class SalesforceControllerTest {
+    @Mock
+    private ForceApi forceApiMock;
+
+
+    @Test
+    void fetchEventTesting(WireMockRuntimeInfo wmRuntimeInfo) {
+        String responseBody = " {attributes={type=EventLogFile, url=/services/data/v39.0/sobjects/EventLogFile/0AT5i00002bvnnaGAA\n" +
+                "        }, Id=0AT5i00002bvnnaGAA, EventType=ApiTotalUsage, LogFile=/services/data/v39.0/sobjects/EventLogFile/0AT5i00002bvnnaGAA/LogFile, LogDate=2023-06-02T00: 00: 00.000+0000, LogFileLength=683.0\n" +
+                "    }";
+        String apiUrl = "/fetchEvent";
+
+        //Define stub
+        stubFor(get(apiUrl).willReturn(ok(responseBody)));
+
+        //Hit API and check response
+        String apiResponse = getContent(wmRuntimeInfo.getHttpBaseUrl() + apiUrl);
+        assertEquals(apiResponse, responseBody);
+
+        //Verify API is hit
+        verify(getRequestedFor(urlEqualTo(apiUrl)));
+    }
+
+    private String getContent(String url) {
+
+        TestRestTemplate testRestTemplate = new TestRestTemplate();
+        return testRestTemplate.getForObject(url, String.class);
+    }
+}
+
+
